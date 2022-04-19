02e3393f1d7fc2a2aa27f4da9c784a12a349bf57
User <rashidbekraximov21@gmail.com>
2022-04-19 23:30:17 +0500

    Comitting

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'font-awesome/css/font-awesome.min.css'
+
+require('./assets/css/tooplate.css')
+require('./assets/css/fontawesome.min.css')
+require('./assets/css/bootstrap.min.css')
+
+import 'chart.js/dist/chart.min.js'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
